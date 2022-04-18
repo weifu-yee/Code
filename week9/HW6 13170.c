dc7469b1a0be090bcf6921ae53fca95f8896da7b
weifu-yee <chenweifu9999@gmail.com>
2022-04-19 00:02:16 +0800

    4/19

--- week9/HW6 13170.c
+++ week9/HW6 13170.c
@@ -5,7 +5,7 @@ int main() {
     int i = 0,pn = 1,temp = 0,sum = 0;
     while(1){
         if (*(arr+i) >= 48 && *(arr+i) <= 57){
-            temp *= 10 , temp += (pn)? (*(arr+i) - 48) : (48 - *(arr+i));
+            temp *= 10 , temp = temp + ((pn)? (*(arr+i) - 48) : (48 - *(arr+i)));
         }else{
             sum += temp , temp = 0 , pn = (*(arr+i) == 43)? 1 : 0 ;
             if (*(arr+i) == '\0') break;
