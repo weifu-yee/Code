7a01500abe79866b920be9586d86ebe940bfd682
weifu-yee <chenweifu9999@gmail.com>
2022-04-19 22:23:05 +0800

    333

---
+++ week10/HW7_20220419.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Press 1 to show the arrangement by Name!");
+    printf("Press 2 to show the arrangement by Name!");
+    printf("Please input");
+}
