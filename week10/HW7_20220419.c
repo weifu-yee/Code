fa00fe7607a9f877ff04dcf89bf32250d60ffb73
weifu-yee <chenweifu9999@gmail.com>
2022-04-22 22:19:16 +0800

    4/22

--- week10/HW7_20220419.c
+++ week10/HW7_20220419.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct student{
+    int number;
+    char[10];
+    int score;
+    struct student* next;
+}STU;
+
 int main(){
-    printf("Press 1 to show the arrangement by Name!");
-    printf("Press 2 to show the arrangement by Name!");
+    FILE *input = fopen("input.txt","r");
+
+    fscanf(input,"%d %s %d",&)
+
+
+
+
+    printf("Press 1 to show the arrangement by Name!\n");
+    printf("Press 2 to show the arrangement by Name!\n");
     printf("Please input");
 }
+
