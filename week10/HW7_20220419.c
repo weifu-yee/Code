1b125c7cc7c76a2398c16d9fa8df64f553d0cd3e
weifu-yee <chenweifu9999@gmail.com>
2022-04-25 21:25:08 +0800

    4/25

--- week10/HW7_20220419.c
+++ week10/HW7_20220419.c
@@ -3,21 +3,34 @@
 
 typedef struct student{
     int number;
-    char[10];
+    char name[10];
     int score;
     struct student* next;
 }STU;
 
 int main(){
     FILE *input = fopen("input.txt","r");
+    STU *head;
+    head = malloc (sizeof (STU));
+    // head -> next = malloc (sizeof (STU));
+    STU *current;
+    current = head;
+    while(1){
+        if (fscanf(input,"%d %s %d",current -> number,current -> name,current -> score) != EOF){
+        current = current -> next;
+        current -> next = malloc (sizeof (STU));
+        }else{
+            free(current -> next);
+            current -> next = NULL;
+        }
+    }
 
-    fscanf(input,"%d %s %d",&)
 
 
 
-
-    printf("Press 1 to show the arrangement by Name!\n");
-    printf("Press 2 to show the arrangement by Name!\n");
-    printf("Please input");
+    printf("Press '1' to show the arrangement by number!\n");
+    printf("Press '2' to show the arrangement by name!\n");
+    printf("Press '3' to show the arrangement by score!\n");
+    printf("Please press ");
 }
 
