30b320f7c9d6567890fd8b9a5248a266dffe0834
weifu-yee <chenweifu9999@gmail.com>
2022-04-26 21:50:28 +0800

    4/26

--- week10/HW7_20220419.c
+++ week10/HW7_20220419.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 typedef struct student{
     int number;
     char name[10];
@@ -8,63 +7,59 @@ typedef struct student{
     struct student* next;
 }STU;
 
-void visit(STU* n);
+void visitlist(STU* n);
 
 int main(){
     FILE *input = fopen("input.txt","r");
-    // STU *head = NULL, *New = NULL;
-    // int num, score, nop = 0;
-    // char name[10] = {0};
-    int num;
-    fscanf(input,"%d",&num);
+    STU *head = NULL, *new_stu = NULL;
+    int num, score, nop=0, temp=0;
+    char* name[10];
+    char* name_ptr = name;
 
-    printf("%d",num);
+    // fscanf(input,"%d",&num);
+    // fscanf(input,"%s",name);
+    // fscanf(input,"%d",&score);
+    while( fscanf(input,"%d%s%d",&num,name,&score) == 3){
+        new_stu = (STU*) malloc( sizeof( STU));
+        new_stu->number = num;
+        new_stu->name[0] = name_ptr;
+        new_stu->score = score;
+        new_stu->next = head;
+        head = new_stu;
+        nop ++;
+    }
+        visitlist(head);
+    // }
+
+    // STU* arr1[nop];
+    // STU* arr2[nop];
+    // STU* arr3[nop];
+
+    // STU* n = head;
+    // for( int i = 0; i < nop; i++){
+    //     arr1[i] = n;
+    //     arr2[i] = n;
+    //     arr3[i] = n;
+    //     n = n->next;
+    // }
+    // for( int i = 0; i < nop; i++){
+    //     printf("%d\t",arr1[i]->number);
+    // }
+    // printf("Press '1' to show the arrangement by rollnumber!\n");
+    // printf("Press '2' to show the arrangement by name!\n");
+    // printf("Press '3' to show the arrangement by score!\n");
+    // printf("Please press ");
+
+    // printf("%d\n",num);
+    // printf("%s\n",name_ptr);
+    // printf("%d\n",score);
     fclose(input);
     return 0;
 }
-//     while(1){
-//         fscanf(input,"%d %s %d",&num,name,&score);
-//         printf("%d %s %d",num,name,score);
-//         if (fscanf(input,"%d",&num) == EOF){
-//             break;
-//         }
-//         New = (STU*) malloc( sizeof( STU));
-//         New->number = num;
-//         char* aa = New->name;
-//         aa = name;
-//         New->score = score;
-//         New->next = head;
-//         head = New;
-//         nop ++;
-//     }
 
-//     STU* arr1[nop];
-//     STU* arr2[nop];
-//     STU* arr3[nop];
-
-//     STU* n = head;
-//     for( int i = 0; i < nop; i++){
-//         arr1[i] = n;
-//         arr2[i] = n;
-//         arr3[i] = n;
-//         n = n->next;
-//     }
-//     for( int i = 0; i < nop; i++){
-//         printf("%d\t",arr1[i]->number);
-//     }
-
-
-
-
-//     printf("Press '1' to show the arrangement by number!\n");
-//     printf("Press '2' to show the arrangement by name!\n");
-//     printf("Press '3' to show the arrangement by score!\n");
-//     printf("Please press ");
-// }
-
-// void visit(STU* n){
-//     while(n != NULL){
-//         printf("%d %s %d",(n->number),n->name,(n->score));
-//         n = n->next;
-//     }
-// }
+void visitlist(STU* n){
+    while(n != NULL){
+        printf("%d %d\n",n->number,n->score);
+        n = n->next;
+    }
+}
