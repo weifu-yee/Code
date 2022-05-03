d01d6f29ef08a80f311fe7a11916c00aaff91c21
weifu-yee <chenweifu9999@gmail.com>
2022-05-03 23:52:32 +0800

    5/3

--- week11/HW7_20220419.c
+++ week11/HW7_20220419.c
@@ -18,32 +18,32 @@ int main(){
     while( fscanf(input,"%d%s%d",&num,name,&score) == 3){
         new_stu = (STU*) malloc( sizeof( STU));
         new_stu->number = num;
-        char* new_name = malloc( 10*sizeof( char));     //含字串結尾最多10個
+        char* new_name = malloc( 10*sizeof( char));     //�t�r�굲���̦h10��
         for( int i = 0; i < 10; i++)    *(new_name + i) = *(name + i);
         new_stu->name = new_name;
         new_stu->score = score;
         new_stu->next = head;
         head = new_stu;
-        nop ++;             //紀錄人數
+        nop ++;             //�����H��
     }
-    head = bubblesort(head,nop);        //進行泡沫排序
+    head = bubblesort(head,nop);        //�i��w�j�Ƨ�
 
     STU* arrN[nop];
     STU* arrS[nop];
     STU* n = head;
-    for( int i = 0; i < nop; i++){      //建立2個新的陣列 記錄指標指向linked list
-        arrN[i] = n;                    //姓名排序
-        arrS[i] = n;                    //學號排序
+    for( int i = 0; i < nop; i++){      //�إ�2�ӷs���}�C �O�����Ы��Vlinked list
+        arrN[i] = n;                    //�m�W�Ƨ�
+        arrS[i] = n;                    //�Ǹ��Ƨ�
         n = n->next;
     }
-    
-    STU* stuptr_temp;       //暫存指標
-    int a,b;        //另存兩個變數以比較英文排序(考慮大小寫)
-    for( int i = 0; i < nop -1; i++){       //這個for迴圈排姓名
+
+    STU* stuptr_temp;       //�Ȧs����
+    int a,b;        //�t�s����ܼƥH����^��Ƨ�(�Ҽ{�j�p�g)
+    for( int i = 0; i < nop -1; i++){       //�o��for�j��Ʃm�W
         for( int j = 0; j < nop -1 -i; j++){
             int k;
-            for( k = 0; k < 9; k++){        //名字不超過9個字母，最多往下比到9個
-                a = *(arrN[j]->name + k) - 65;      //大寫小寫轉換為相同數字比較
+            for( k = 0; k < 9; k++){        //�W�r���W�L9�Ӧr���A�̦h���U���9��
+                a = *(arrN[j]->name + k) - 65;      //�j�g�p�g�ഫ���ۦP�Ʀr���
                 a = ((a-25)>0)? a-32 : a;           //~
                 b = *(arrN[j+1]->name + k) - 65;    //~
                 b = ((b-25)>0)? b-32 : b;           //~
@@ -51,7 +51,7 @@ int main(){
                     stuptr_temp = arrN[j];
                     arrN[j] = arrN[j+1];
                     arrN[j+1] = stuptr_temp;
-                    break;        
+                    break;
                 }else if( a < b )   break;
             }
             if(k == 9){
@@ -63,13 +63,13 @@ int main(){
             }
         }
     }
-    for( int i = 0; i < nop -1; i++){           //這個for迴圈排成績
+    for( int i = 0; i < nop -1; i++){           //�o��for�j��Ʀ��Z
         for( int j = 0; j < nop -1 -i; j++){
             if( arrS[j]->score > arrS[j+1]->score){
                     stuptr_temp = arrS[j];
                     arrS[j] = arrS[j+1];
                     arrS[j+1] = stuptr_temp;
-            }else if(arrS[j]->score == arrS[j+1]->score){       //分數一樣比學號
+            }else if(arrS[j]->score == arrS[j+1]->score){       //���Ƥ@�ˤ�Ǹ�
                 if( arrS[j]->number > arrS[j+1]->number){
                     stuptr_temp = arrS[j];
                     arrS[j] = arrS[j+1];
@@ -82,42 +82,43 @@ int main(){
     printf("Key '1' to show the arrangement by rollNumber!\n");
     printf("Key '2' to show the arrangement by studentName!\n");
     printf("Key '3' to show the arrangement by score!\n");
+    printf("Key 'q' to exit!\n");
     char cmd = 0;
     int while_time = 0;
-    while(cmd != 'q'){          //指令迴圈
+    while(cmd != 'q'){          //���O�j��
         printf((while_time)?"\nPlease key a command and press the 'Enter' again : ":"Please key a command and press the 'Enter' : ");
         scanf("\n%c",&cmd);
         switch( cmd){
             case '1' :
-                printf("\nBy rollNumber ~ ~ ~\n");
+                printf("\n\tBy rollNumber ~\n");
                 visitlist(head);
                 break;
             case '2' :
-                printf("\nBy studentName ~ ~ ~\n");
+                printf("\n\tBy studentName ~\n");
                 for( int i = 0; i < nop; i++)   printf("%d %s %d\n",arrN[i]->number,arrN[i]->name,arrN[i]->score);
-                break;   
+                break;
             case '3' :
-                printf("\nBy score ~ ~ ~\n");
+                printf("\n\tBy score ~\n");
                 for( int i = 0; i < nop; i++)   printf("%d %s %d\n",arrS[i]->number,arrS[i]->name,arrS[i]->score);
                 break;
             case 'q' :
                 break;
             default :
-                printf("\nThis command is not valid ! ! ! ! ! ! !\n");
+                printf("\n\tThis command is not valid ! ! ! ! ! ! !\n");
                 break;
         }
-        while_time =1;          //紀錄是否為首次printf
+        while_time =1;          //�����O�_������printf
     }
     fclose(input);
     return 0;
 }
-void visitlist(STU* n){             //printf出 linked list
+void visitlist(STU* n){             //printf�X linked list
     while(n != NULL){
         printf("%d %s %d\n",n->number,n->name,n->score);
         n = n->next;
     }
 }
-STU* bubblesort(STU* head,int nop){         //泡沫排序linked list
+STU* bubblesort(STU* head,int nop){         //�w�j�Ƨ�linked list
     STU *temp = head;
     for(int i = nop - 1; i > 0; i--){
         STU  *prev = head, *curr = head;
