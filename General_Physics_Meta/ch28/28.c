4bd3ecf32efdf13fcfe5b0bd2c1ef8cdc37353d6
weifu-yee <chenweifu9999@gmail.com>
2022-05-06 15:38:28 +0800

    4/5

--- General_Physics_Meta/ch28/28.c
+++ General_Physics_Meta/ch28/28.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int q1(FILE* file){
+
+}
 int main(){
+    FILE* file = fopen( "D:\Code\General_Physics_Meta\ch28\value.txt", "r");
     
+    printf("%d\t",q1(&file));
+    printf("%d\t",q2(&file));
+    printf("%d\t",q3(&file));
+    printf("%d\t",q4(&file));
+    printf("%d\t",q5(&file));
+    printf("%d\t",q6(&file));
+
+    fclose( file);
+    return 0;
 }
