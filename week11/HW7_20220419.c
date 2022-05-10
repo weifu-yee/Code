67f9f99b615506d859057e8e6ef6c83e0951bcac
weifu-yee <chenweifu9999@gmail.com>
2022-05-11 01:25:07 +0800

    5/11

--- week11/HW7_20220419.c
+++ week11/HW7_20220419.c
@@ -18,35 +18,35 @@ int main(){
     while( fscanf(input,"%d%s%d",&num,name,&score) == 3){
         new_stu = (STU*) malloc( sizeof( STU));
         new_stu->number = num;
-        char* new_name = malloc( 10*sizeof( char));     //�t�r�굲���̦h10��
+        char* new_name = malloc( 10*sizeof( char));
         for( int i = 0; i < 10; i++)    *(new_name + i) = *(name + i);
         new_stu->name = new_name;
         new_stu->score = score;
         new_stu->next = head;
         head = new_stu;
-        nop ++;             //�����H��
+        nop ++;
     }
-    head = bubblesort(head,nop);        //�i��w�j�Ƨ�
+    head = bubblesort(head,nop); 
 
     STU* arrN[nop];
     STU* arrS[nop];
     STU* n = head;
-    for( int i = 0; i < nop; i++){      //�إ�2�ӷs���}�C �O�����Ы��Vlinked list
-        arrN[i] = n;                    //�m�W�Ƨ�
-        arrS[i] = n;                    //�Ǹ��Ƨ�
+    for( int i = 0; i < nop; i++){      
+        arrN[i] = n;                    
+        arrS[i] = n;                    
         n = n->next;
     }
 
-    STU* stuptr_temp;       //�Ȧs����
-    int a,b;        //�t�s����ܼƥH����^��Ƨ�(�Ҽ{�j�p�g)
-    for( int i = 0; i < nop -1; i++){       //�o��for�j��Ʃm�W
+    STU* stuptr_temp;       
+    int a,b;        
+    for( int i = 0; i < nop -1; i++){       
         for( int j = 0; j < nop -1 -i; j++){
             int k;
-            for( k = 0; k < 9; k++){        //�W�r���W�L9�Ӧr���A�̦h���U���9��
-                a = *(arrN[j]->name + k) - 65;      //�j�g�p�g�ഫ���ۦP�Ʀr���
-                a = ((a-25)>0)? a-32 : a;           //~
-                b = *(arrN[j+1]->name + k) - 65;    //~
-                b = ((b-25)>0)? b-32 : b;           //~
+            for( k = 0; k < 9; k++){        
+                a = *(arrN[j]->name + k) - 65;      
+                a = ((a-25)>0)? a-32 : a;          
+                b = *(arrN[j+1]->name + k) - 65;    
+                b = ((b-25)>0)? b-32 : b;           
                 if( a > b ){
                     stuptr_temp = arrN[j];
                     arrN[j] = arrN[j+1];
@@ -63,13 +63,13 @@ int main(){
             }
         }
     }
-    for( int i = 0; i < nop -1; i++){           //�o��for�j��Ʀ��Z
+    for( int i = 0; i < nop -1; i++){        
         for( int j = 0; j < nop -1 -i; j++){
             if( arrS[j]->score > arrS[j+1]->score){
                     stuptr_temp = arrS[j];
                     arrS[j] = arrS[j+1];
                     arrS[j+1] = stuptr_temp;
-            }else if(arrS[j]->score == arrS[j+1]->score){       //���Ƥ@�ˤ�Ǹ�
+            }else if(arrS[j]->score == arrS[j+1]->score){     
                 if( arrS[j]->number > arrS[j+1]->number){
                     stuptr_temp = arrS[j];
                     arrS[j] = arrS[j+1];
@@ -85,7 +85,7 @@ int main(){
     printf("Key 'q' to exit!\n");
     char cmd = 0;
     int while_time = 0;
-    while(cmd != 'q'){          //���O�j��
+    while(cmd != 'q'){        
         printf((while_time)?"\nPlease key a command and press the 'Enter' again : ":"Please key a command and press the 'Enter' : ");
         scanf("\n%c",&cmd);
         switch( cmd){
@@ -107,18 +107,18 @@ int main(){
                 printf("\n\tThis command is not valid ! ! ! ! ! ! !\n");
                 break;
         }
-        while_time =1;          //�����O�_������printf
+        while_time =1;      
     }
     fclose(input);
     return 0;
 }
-void visitlist(STU* n){             //printf�X linked list
+void visitlist(STU* n){            
     while(n != NULL){
         printf("%d %s %d\n",n->number,n->name,n->score);
         n = n->next;
     }
 }
-STU* bubblesort(STU* head,int nop){         //�w�j�Ƨ�linked list
+STU* bubblesort(STU* head,int nop){        
     STU *temp = head;
     for(int i = nop - 1; i > 0; i--){
         STU  *prev = head, *curr = head;
