917afe26171391742342ac708cf8a0dd1cd98a3d
weifu_yee <chenweifu9999@gmail.com>
2023-03-26 15:46:02 +0800

    0326 second

--- introduction to programing/factor_counter.cpp
+++ introduction to programing/factor_counter.cpp
@@ -1,19 +1,32 @@
 #include <stdio.h>
 
 int main(){
+    //estblish a prime number array
+    int prime[2000] = {0};
+    int prime_number = 0;
+    for(int i = 2; i < 2000; i++){
+        if(prime[i] == 0){
+            prime_number++;
+            for(int j = i + i; j < 2000; j += i){
+                prime[j] = 1;
+            }
+        }
+    }
     int n;
     scanf("%d",&n);
     for(int i = 0; i < n; i++){
-        //to find out the most factor number in the range
+        //to find out the most factor number in the range use the prime number array
         int l;  int r;
         scanf("%d %d",&l,&r);
         int max = 0;
         int max_number = 0;
         for(int j = l; j <= r; j++){
             int factor = 0;
-            for(int k = 1; k <= j; k++){
-                if(j % k == 0){
-                    factor ++;
+            for(int k = 0; k <= j; k++){
+                if( prime[k] != 0){ //###
+                    if(j%k==0){ //###
+                        factor ++;
+                    }
                 }
             }
             if(factor > max){
@@ -21,6 +34,7 @@ int main(){
                 max_number = j;
             }
         }
-    printf("%d\n",max_number);
+        printf("%d\n",max_number);
     }
+    return 0;
 }
